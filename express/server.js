483818ce1e20757172f17f768379711009bfa66f
Festus Omole <festusomole14@gmail.com>
2020-06-07 13:37:36 +0100

    making changes to endpoints

--- express/server.js
+++ express/server.js
@@ -8,10 +8,12 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 router.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js!</h1>");
+  res.write("<h1>Hello to my first backend deploy</h1>");
   res.end();
 });
-router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.get("/another", (req, res) =>
+  res.json({ route: req.originalUrl, another: "data" })
+);
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
